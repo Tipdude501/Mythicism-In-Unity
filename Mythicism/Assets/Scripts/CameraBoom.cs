ac118003704bc0c5c0c6b73628231695cb3831b7
Reilly da Silva <reillydasilva@gmail.com>
2022-06-05 13:53:19 -0400

    Add mouse movement to follow camera

--- Mythicism/Assets/Scripts/CameraBoom.cs
+++ Mythicism/Assets/Scripts/CameraBoom.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class CameraBoom : MonoBehaviour
 {
     #region Members
+    [Header("Camera Booom Data")]
     [SerializeField] public Transform target;
-    [SerializeField] public float followSmoothTime; 
+    [SerializeField] public float followSmoothTime;
+    [SerializeField] public float yawSpeed;
+    [SerializeField] public float pitchSpeed;
+
+    [Header("Input Data")]
+    private float deltaMouseX;
+    private float deltaMouseY;
     #endregion
 
 
@@ -14,11 +21,13 @@ public class CameraBoom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        deltaMouseX = Input.GetAxis("Mouse X");
+        deltaMouseY = Input.GetAxis("Mouse Y");
     }
 
     private void FixedUpdate()
     {
+        // Follow target
         float velocity = 0;
 
         Vector3 newPosition = transform.position;
@@ -27,6 +36,15 @@ public class CameraBoom : MonoBehaviour
         newPosition.z = Mathf.SmoothDamp(newPosition.z, target.position.z, ref velocity, followSmoothTime);
 
         transform.position = newPosition;
+
+
+        // Rotate with mouse movement
+        transform.Rotate(0, deltaMouseX * Time.deltaTime * yawSpeed, 0);
+        transform.Rotate(-deltaMouseY * Time.deltaTime * pitchSpeed, 0, 0);
+        Vector3 newRotation = transform.rotation.eulerAngles;
+        newRotation.z = 0;
+      
+        transform.rotation = Quaternion.Euler(newRotation);
     }
 
 
